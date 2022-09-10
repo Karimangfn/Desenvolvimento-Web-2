05ea47d5999222ae229b1f93ac8167dc050a3a64
Kariman <86572034+Karimangfn@users.noreply.github.com>
2022-09-10 01:33:11 -0300

    Update main.js

--- Atividades/A3/src/js/main.js
+++ Atividades/A3/src/js/main.js
@@ -1,14 +1,28 @@
-import mask from './modules/mask.js'
-import valid from './modules/valid.js'
+import { masks } from './modules/mask.js'
+import { valids } from './modules/valid.js'
 
+document.querySelectorAll('input').forEach(($input) => {
+    const field = $input.dataset.js
 
+    $input.addEventListener('input', (e) => {
+        e.target.value = masks[field](e.target.value)
+    }, false)
+})
 
+function checkInputs() {
+    document.querySelectorAll('input').forEach(($input) => {
+    const field = $input.dataset.js
 
+    $input.addEventListener('input', (e) => {
+        let isValid = valids[field](e.target.value)
+        if (isValid) {
+            e.target.classList.remove('errorInput')
+        } else {
+            e.target.classList.add('errorInput')
+        }
+        
+    }, false)
+    })
+}
 
-
-
-
-
-
-
-
+checkInputs()
